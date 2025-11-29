a61af60e997805b9708d0f30739928f72c61bdcd
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-11-29 20:36:38 +0100

    strings_2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,11 @@ console.log(name)
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+const number = 15;
+const text = "stringa";
+const bool = true;
+console.log(number,text,bool)
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
